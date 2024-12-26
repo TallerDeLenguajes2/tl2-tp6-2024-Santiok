01211ec2737fbfe1092a1c7ddda7c886428fd768
“Santiago” <santimedinaok.10@gmail.com>
2024-12-26 13:19:47 -0300

    Modificaciones.

--- TP6/Controllers/ProductoController.cs
+++ TP6/Controllers/ProductoController.cs
@@ -3,11 +3,65 @@ using Models;
 using Repositorios;
 namespace Controllers;
 
-
-[ApiController]
-[Route("[controller]")]
-
 public class ProductoController : Controller
 {
+    private readonly ILogger<ProductoController> _logger;
+    private readonly ProductoRepository productoRepository;
+    public ProductoController(ILogger<ProductoController> logger)
+    {
+        _logger = logger;
+        productoRepository = new ProductoRepository("Data Source=BD/Tienda.db;Cache=Shared");
+    }
 
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var productos = productoRepository.ListarProductos();
+        return View(productos);
+    }
+
+
+    [HttpGet]
+    public IActionResult CrearProducto()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult CrearProducto(Producto producto)
+    {
+        productoRepository.CrearProducto(producto);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult ModificarProducto(int id)
+    {
+        return View(productoRepository.ObtenerProductoPorId(id));
+    }
+
+
+    [HttpPost]
+    public IActionResult ModificarProducto(int id, Producto producto)
+    {
+        productoRepository.ModificarProducto(id, producto);
+        return RedirectToAction("Index");
+    }
+
+
+    [HttpGet]
+    public IActionResult EliminarProducto(int id)
+    {
+        return View(productoRepository.EliminarProducto(id));
+    }
+
+
+    [HttpPost]
+    public IActionResult ConfirmarEliminacion(int id)
+    {
+        productoRepository.EliminarProducto(id);
+        return RedirectToAction("Index");
+    }
 }
